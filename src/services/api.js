301ad44d73141b3bf03f756ad13669ad93fdf6d5
hasifasherin <hasifasherincp@gmail.com>
2026-01-16 21:40:28 +0530

    api change

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,9 @@ import axios from "axios";
 import baseURL from "../utils/baseUrl";
 
 const api = axios.create({
-  baseURL, // uses the variable from baseUrl.js
+  baseURL,
 });
 
-// Add JWT token to every request if it exists
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
